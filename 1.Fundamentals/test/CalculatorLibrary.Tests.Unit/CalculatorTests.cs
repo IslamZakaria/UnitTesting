6bea42816f05004aee1b53e20267e16bfc47973e
Islam Mohamed <eslamzakaria293@gmail.com>
2024-09-24 12:11:50 +0300

    feat: xUnit execution model

--- 1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ 1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -4,14 +4,13 @@ namespace CalculatorLibrary.Tests.Unit;
 
 public class CalculatorTests
 {
+    private readonly Calculator _sut = new();
+
     [Fact]
     public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
     {
-        //Arrange
-        var calculator = new Calculator();
-
         //Act
-        var result = calculator.Add(5, 4);
+        var result = _sut.Add(5, 4);
 
         //Assert
         Assert.Equal(9, result);
