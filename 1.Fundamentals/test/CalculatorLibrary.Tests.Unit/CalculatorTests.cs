74e53e8ca7195c9a3d19d58ab3cadb33e7333c10
Islam Mohamed <eslamzakaria293@gmail.com>
2024-09-24 12:08:57 +0300

    feat: added my first test method

--- 1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
+++ 1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -1,6 +1,19 @@
-﻿namespace CalculatorLibrary.Tests.Unit;
+﻿using Xunit;
+
+namespace CalculatorLibrary.Tests.Unit;
 
 public class CalculatorTests
 {
+    [Fact]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        //Arrange
+        var calculator = new Calculator();
 
+        //Act
+        var result = calculator.Add(5, 4);
+
+        //Assert
+        Assert.Equal(9, result);
+    }
 }
